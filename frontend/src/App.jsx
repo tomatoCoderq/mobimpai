2291a81837b0c6c6978ed47f08b6d4725591fe66
Mukhin-I <ilyamuhin357@gmail.com>
2026-05-08 12:49:28 +0300

    add: partially completed main screen

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import logo from './assets/MobImpAI.svg'
 import main from './assets/main.png'
 import footer from './assets/footer.png'
 import cta from './assets/cta.svg'
-import cardBg from '/assets/card-map-bg.png'
+import cardBg from './assets/card-map-bg.png'
 import mapBg from './assets/bg-map.png'
 import './App.css'
 
@@ -18,7 +18,34 @@ function App() {
 function Header() {
   return (
       <>
-      <p>Hello World!</p>
+        <div className="header-wrapper">
+          <div className="container">
+            <div className="menu-top">
+              <img src={logo} alt="logo" />
+              <div className="menu-controls">
+                <ul className="menu">
+                  <li><a href="#">Home</a></li>
+                  <li><a href="#">About</a></li>
+                  <li><a href="#">Mission</a></li>
+                </ul>
+                <a href="#" className="cta">Try
+                  <img src={cta} alt="redirect" />
+                </a>
+              </div>
+            </div>
+
+            <div className="offer-wrapper">
+              <div className="offer-left">
+                <h4>The City.</h4>
+                <h1>Unclocked.</h1>
+              </div>
+              <div className="offer-right">
+                <h3>Navigation designed for every curb, every step, and every journey.</h3>
+                <p>Redefining urban mobility with high-precision routing that sees what others miss.</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </>
   );
 }
