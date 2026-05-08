2aa3a144decbd5654365c9665a2c7eaaec9140c9
Mukhin-I <ilyamuhin357@gmail.com>
2026-05-08 11:20:07 +0300

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,25 @@
+import logo from './assets/MobImpAI.svg'
+import main from './assets/main.png'
+import footer from './assets/footer.png'
+import cta from './assets/cta.svg'
+import cardBg from '/assets/card-map-bg.png'
+import mapBg from './assets/bg-map.png'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
+
+function Header() {
+  return (
+      <>
+      </>
+  );
+}
+
+export default App
